4ab0c319d7d6c63fca3adc11d7c2325d9ae63b72
Radhi Rasho <Radhi.H@outlook.com>
2024-02-29 17:51:59 +0000

    added global request tracker with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import type { RandomUser, Repo } from "@/types";
-import { queryOptions, useQueries, useQuery } from "@tanstack/react-query";
+import { queryOptions, useIsFetching, useQueries, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function Home() {
 	const [count, setCount] = useState(1);
+	const isFetching = useIsFetching();
+
 	const { isPending, error, data, isSuccess, isError } = useQuery<Repo>({
 		queryKey: ["repoData", count], // Query Keys are similar to Dependency Arrays in useEffect
 		queryFn: async ({ queryKey }): Promise<Repo> => {
@@ -28,36 +30,53 @@ export default function Home() {
 			return res.json();
 		},
 		staleTime: 3000, // This is the time in ms that the query will be considered fresh
+		gcTime: 3000, // This is the time in ms that the query will be garbage collected
 		// enabled: count > 5, // This is a way to disable the query until it's dependencies match a certain criteria
 	});
 
 	const { data: names } = useQuery({
-		queryKey: ["users", count],
+		queryKey: ["user", count],
 		queryFn: async (): Promise<RandomUser> => {
-			return fetch(`https://randomuser.me/api?results=${count}`).then((res) => res.json());
+			const res = await fetch(`https://randomuser.me/api?results=${count}`);
+			const data = await res.json();
+			return data;
 		},
 		select: (data) => data.results.map((x) => `${x.name.first} ${x.name.last}`),
 		staleTime: 3000, // This is the time in ms that the query will be considered fresh
+		gcTime: 3000, // This is the time in ms that the query will be garbage collected
 	});
 
-	const users = useQueries({
+	const usersQ = useQueries({
 		queries: names
 			? names.map((name) => {
 					return queryOptions({
-						queryKey: ["user", name],
-						queryFn: async () => {
-							return name;
-						},
+						queryKey: ["users", name],
+						queryFn: async () => name,
 						staleTime: 3000,
+						gcTime: 3000,
 					});
 			  })
 			: [],
 	});
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-red-500">
-			{isPending && <div>Loading...</div>}
-			{isError && <div>Error: {error.message}</div>}
+		<main className="flex min-h-screen flex-col items-center justify-evenly p-24 bg-black text-white">
+			<div className="absolute top-10 right-10">
+				{isFetching > 0 ? (
+					<div className="flex text-red-500 w-14 items-center justify-between">
+						{isFetching}{" "}
+						<div className="bg-red-500 rounded-full flex justify-center items-center h-6 w-6 shadow-[0_0_25px] shadow-red-500">
+							<div className="bg-red-500 rounded-full h-5 w-5 motion-safe:animate-ping-slow">{""}</div>
+						</div>
+					</div>
+				) : (
+					<div className="flex text-red-500 w-14 items-center justify-between">
+						{isFetching} <div className="bg-red-500 rounded-full flex justify-center items-center h-6 w-6">{""}</div>
+					</div>
+				)}
+			</div>
+			{/* {isPending && <span>Loading...</span>}
+			{isError && <span className="text-red-500">Error: {error.message}</span>}
 			{isSuccess && (
 				<div>
 					<h1>{data.name}</h1>
@@ -65,9 +84,17 @@ export default function Home() {
 					<strong>👀 {data.subscribers_count}</strong> <strong>✨ {data.stargazers_count}</strong>{" "}
 					<strong>🍴 {data.forks_count}</strong>
 				</div>
+			)} */}
+			{usersQ.length > 0 && (
+				<div className={`overflow-hidden ${usersQ?.length > 16 ? "hover:overflow-y-scroll" : ""} h-96 w-56`}>
+					{usersQ?.length > 0 && usersQ?.map((user, i) => <div key={`user-${i * 2}`}>{user.data}</div>)}
+				</div>
 			)}
-			{users.length > 0 && users?.map((user, i) => <div key={`user-${i * 2}`}>{user.data}</div>)}
-			<button type="button" onClick={() => setCount((c) => c + 1)}>
+			<button
+				type="button"
+				className="border border-white w-32 h-10 rounded-full active:animate-in active:translate-y-1"
+				onClick={() => setCount((c) => c + 1)}
+			>
 				Increment
 			</button>
 		</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,7 @@ const config = {
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				"ping-slow": "ping 1.5s cubic-bezier(0.6, 1, 0.5, 0.5) infinite",
 			},
 		},
 	},
