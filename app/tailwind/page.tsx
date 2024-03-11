8ed124054936d5349ea249c8482be81dc9b68ea1
Radhi Rasho <Radhi.H@outlook.com>
2024-03-11 16:28:22 +0000

    updates, adn more cleaning

--- app/tailwind/page.tsx
+++ app/tailwind/page.tsx
@@ -1,8 +1,8 @@
 export default function TailWind() {
 	return (
 		<main className="flex h-full items-center justify-center bg-dark">
-			<ul className="flex w-6/12 justify-between *:flex *:w-full *:justify-center *:gap-4 *:rounded-full *:border *:border-blue-600 *:bg-blue-400 *:bg-opacity-25 *:shadow-blue-700 *:shadow-md">
-				<li>1</li>
+			<ul className="flex w-3/6 *:size-8 justify-between *:flex *:min-w-24 *:justify-center *:items-center *:rounded-full *:border *:border-blue-600 *:bg-blue-300 *:bg-opacity-20 *:shadow-blue-700 *:shadow-md">
+				<li className="hover:shadow-lg">1</li>
 				<li>2</li>
 				<li>3</li>
 				<li>4</li>
